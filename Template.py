7af2a84003972a678b8a62bdd0d60b1b0d9d734b
Garrett <g.romo2@students.clark.edu>
2020-05-20 12:16:06 -0700

    Mod5 lab 2 complete

--- Template.py
+++ Template.py
@@ -1,7 +1,7 @@
 """
 CTEC 121
-<your name>
-<assignment/lab name>
+<Garrett>
+<Mod 5 Lab 2>
 <assignment/lab description
 """
 
@@ -10,8 +10,51 @@ Input(s): list/description
 Process: description of what function does
 Output: return value and description
 """
+'''
+def fingerFamily(name):
+    print()
+    print(name, "finger,", name, "finger, where are you?")
+    print("Here I am, here I am. How do you do?")
+    print()
 
 def main():
-    # code here
+    print()
+    fingerFamily("Daddy")
+    print()
+    fingerFamily("Mommy")
+    print()
+    fingerFamily("Brother")
+    print()
+    fingerFamily("Sister")
+    print()
+    fingerFamily("Baby")
+    print()
+'''
 
-main()    
+
+
+def ageRange(age):
+    if age <= 1:
+        return "I"
+    elif 0 > age:
+        return "U"
+    elif 1 < age < 13:
+        return "C"
+    elif 13 < age < 18:
+        return "T"
+    elif 18 < age <= 120:
+        return "A"
+    elif age > 120:
+        return "U"
+
+def unitTest():
+    print(ageRange(-1))
+    print(ageRange(0))
+    print(ageRange(3))
+    print(ageRange(12))
+    print(ageRange(15))
+    print(ageRange(19))
+    print(ageRange(121))
+
+unitTest()
+#main()    
